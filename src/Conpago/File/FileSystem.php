7a453d5dbd1b78c5a479a211592e85994392be09
Bartosz Gołek <bartosz.golek@gmail.com>
2017-02-05 21:20:09 +0100

    missing interface

--- src/Conpago/File/FileSystem.php
+++ src/Conpago/File/FileSystem.php
@@ -1,81 +1,86 @@
 <?php
-	/**
-	 * Created by PhpStorm.
-	 * User: bgolek
-	 * Date: 2014-10-10
-	 * Time: 07:57
-	 */
+/**
+ * Created by PhpStorm.
+ * User: bgolek
+ * Date: 2014-10-10
+ * Time: 07:57
+ */
 
-	namespace Conpago\File;
+namespace Conpago\File;
 
 
-	use Conpago\File\Contract\IFileSystem;
+use Conpago\File\Contract\IFileSystem;
 
-	class FileSystem implements IFileSystem
-	{
+class FileSystem implements IFileSystem
+{
 
-		function includeFile($filePath)
-		{
-			return include $filePath;
-		}
+    function includeFile($filePath)
+    {
+        return include $filePath;
+    }
 
-		function glob($pattern, $flags = null)
-		{
-			return glob($pattern, $flags);
-		}
+    function glob($pattern, $flags = null)
+    {
+        return glob($pattern, $flags);
+    }
 
-		function realPath($path)
-		{
-			return realpath($path);
-		}
+    function realPath($path)
+    {
+        return realpath($path);
+    }
 
-		function getFileContent($filename)
-		{
-			return file_get_contents($filename);
-		}
+    function getFileContent($filename)
+    {
+        return file_get_contents($filename);
+    }
 
-		function setFileContent($filename, $data)
-		{
-			file_put_contents($filename, $data);
-		}
+    function setFileContent($filename, $data)
+    {
+        file_put_contents($filename, $data);
+    }
 
-		function requireOnce($filePath)
-		{
-			require_once $filePath;
-		}
+    function requireOnce($filePath)
+    {
+        require_once $filePath;
+    }
 
-		function requireFile($filePath)
-		{
-			require $filePath;
-		}
+    function requireFile($filePath)
+    {
+        require $filePath;
+    }
 
-		private function getClassName($filePath)
-		{
-			$className = basename($filePath, '.php');
-			$namespace = $this->getNameSpace($filePath);
+    private function getClassName($filePath)
+    {
+        $className = basename($filePath, '.php');
+        $namespace = $this->getNameSpace($filePath);
 
-			$classFullName = strlen($namespace) > 0
-					? '\\'.$namespace.'\\'.$className
-					: '\\'.$className;
+        $classFullName = strlen($namespace) > 0
+            ? '\\' . $namespace . '\\' . $className
+            : '\\' . $className;
 
-			return $classFullName;
-		}
+        return $classFullName;
+    }
 
-		private function getNameSpace($filePath)
-		{
-			$matches = array();
-			if (preg_match('/namespace (.+) *[\{;]{1}/', file_get_contents($filePath), $matches))
-				return $matches[1];
+    private function getNameSpace($filePath)
+    {
+        $matches = array();
+        if (preg_match('/namespace (.+) *[\{;]{1}/', file_get_contents($filePath), $matches))
+            return $matches[1];
 
-			return '';
-		}
+        return '';
+    }
 
-		function loadClass($filePath)
-		{
-			$className = $this->getClassName($filePath);
-			$this->requireOnce($filePath);
+    function loadClass($filePath)
+    {
+        $className = $this->getClassName($filePath);
+        $this->requireOnce($filePath);
 
-			return new $className();
-		}
-	}
+        return new $className();
+    }
+
+    public function createDirectory($pathname, $recursive)
+    {
+        return mkdir($pathname, 0777, $recursive);
+    }
+}
 
